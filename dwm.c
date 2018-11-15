2f69f8b12b6514c677d2d6e6d1ccb6f3a4277df2
Michael Barbour <barbour.michael.0@gmail.com>
2018-11-15 15:46:44 -0500

    Don't focus sticky client unless it was focused before.

--- dwm.c
+++ dwm.c
@@ -94,7 +94,7 @@ struct Client {
 	int basew, baseh, incw, inch, maxw, maxh, minw, minh;
 	int bw, oldbw;
 	unsigned int tags;
-	int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen, issticky;
+	int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen, issticky, wasfocused;
 	Client *next;
 	Client *snext;
 	Monitor *mon;
@@ -814,6 +814,8 @@ void
 focus(Client *c)
 {
 	if (!c || !ISVISIBLE(c))
+		for (c = selmon->stack; c && !(ISVISIBLE(c) && (!c->issticky || (c->issticky && c->wasfocused))); c = c->snext);
+	if (!c)
 		for (c = selmon->stack; c && !ISVISIBLE(c); c = c->snext);
 	if (selmon->sel && selmon->sel != c)
 		unfocus(selmon->sel, 0);
@@ -1504,6 +1506,7 @@ setfocus(Client *c)
 			XA_WINDOW, 32, PropModeReplace,
 			(unsigned char *) &(c->win), 1);
 	}
+	c->wasfocused = 1;
 	sendevent(c, wmatom[WMTakeFocus]);
 }
 
@@ -1828,6 +1831,7 @@ unfocus(Client *c, int setfocus)
 {
 	if (!c)
 		return;
+	c->wasfocused = 0;
 	grabbuttons(c, 0);
 	XSetWindowBorder(dpy, c->win, scheme[SchemeNorm][ColBorder].pixel);
 	if (setfocus) {
